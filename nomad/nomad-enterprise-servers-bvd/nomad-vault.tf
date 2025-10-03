48e27e08b794c5160d43aa73c494c94741ed9c4b
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2025-10-02 20:13:01 -0400

    Refactor comments and update key names in Vault configuration for clarity

--- nomad/nomad-enterprise-servers-bvd/nomad-vault.tf
+++ nomad/nomad-enterprise-servers-bvd/nomad-vault.tf
@@ -10,7 +10,7 @@ resource "vault_mount" "nomad" {
 }
 
 #------------------------------------------------------------------------------
-# Gossip Encryption Key
+# Gossip Encryption Key -> Vault
 #------------------------------------------------------------------------------
 
 # Generate a 32-byte key (AES-256)
@@ -18,18 +18,39 @@ resource "random_id" "nomad_gossip_key" {
   byte_length = 32
 }
 
+# Store the key in Vault
 resource "vault_kv_secret_v2" "nomad_gossip_key" {
   mount               = vault_mount.nomad.path
-  name                = "secret"
+  name                = "gossip_key"
   delete_all_versions = true
   data_json = jsonencode(
     {
-      zip = "zap",
-      foo = "bar"
+      key = random_id.nomad_gossip_key.hex
     }
   )
 }
 
 #------------------------------------------------------------------------------
-# Nomad Configuration Settings
+# Bootstrap ACL Encryption Key -> Vault
 #------------------------------------------------------------------------------
+
+# Generate a 32-byte key (AES-256)
+resource "random_uuid" "nomad_bootstrap_token" {}
+
+
+# Store the key in Vault
+resource "vault_kv_secret_v2" "nomad_bootstrap_token" {
+  mount               = vault_mount.nomad.path
+  name                = "bootstrap_acl_token"
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      key = random_uuid.nomad_bootstrap_token.result
+    }
+  )
+}
+
+#------------------------------------------------------------------------------
+# Nomad TLS Settings
+#------------------------------------------------------------------------------
+
