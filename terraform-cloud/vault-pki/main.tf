73c79fa58eb5f2a5609d433449d4e331a332e4bf
Benjamin Lykins <benjamin.m.lykins@gmail.com>
2025-10-03 22:28:09 -0400

    Refactor PKI configuration to enable Root CA and update issuing/CRL URLs

--- terraform-cloud/vault-pki/main.tf
+++ terraform-cloud/vault-pki/main.tf
@@ -1,8 +1,27 @@
-
-resource "vault_mount" "pki" {
-  path = "pki"
-  type = "pki"
-
-  default_lease_ttl_seconds = 86400
+# Enable a PKI secrets engine for the Root CA
+resource "vault_mount" "pki_root" {
+  path                      = "pki-root"
+  type                      = "pki"
+  description               = "Root PKI"
+  default_lease_ttl_seconds = 315360000
   max_lease_ttl_seconds     = 315360000
+}
+
+# Generate the Root CA (self-signed)
+resource "vault_pki_secret_backend_root_cert" "root" {
+  backend              = vault_mount.pki_root.path
+  type                 = "internal"
+  common_name          = "Nomad Root CA"
+  ttl                  = "87600h" # 10 years
+  key_type             = "rsa"
+  key_bits             = 4096
+  exclude_cn_from_sans = true
+}
+
+# Configure issuing/CRL URLs
+resource "vault_pki_secret_backend_config_urls" "root_urls" {
+  backend                 = vault_mount.pki_root.path
+  issuing_certificates    = ["https://lykins-vault-cluster-public-vault-c88a9e9f.e7ddc59e.z1.hashicorp.cloud:8200/v1/pki-root/ca"]
+  crl_distribution_points = ["https://lykins-vault-cluster-public-vault-c88a9e9f.e7ddc59e.z1.hashicorp.cloud:8200/v1/pki-root/crl"]
+  ocsp_servers            = ["https://lykins-vault-cluster-public-vault-c88a9e9f.e7ddc59e.z1.hashicorp.cloud:8200/v1/pki-root/ocsp"]
 }
